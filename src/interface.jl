6d2383fd2a06d4efcb9729ca17b2ecd34335109b
Marvin Lücke <31667561+lueckem@users.noreply.github.com>
2026-05-21 17:34:57 +0200

    feat: TransitionDistanceProblem

--- src/interface.jl
+++ src/interface.jl
@@ -16,6 +16,85 @@ See also [`compute_embedding`](@ref).
 """
 abstract type AbstractEmbeddingAlgorithm end
 
+const ContiguousData{T<:Real} = Array{T,3}
+const JaggedData{T<:Real} = Vector{Array{T,2}}
+
+abstract type DataLayout end
+struct Contiguous <: DataLayout end
+struct Jagged <: DataLayout end
+
+"""
+    TransitionDistanceProblem{T,W,L}(data; weights)
+
+Struct for the `data` that is used to compute transition distances,
+see [`compute_distances`](@ref).
+
+The `data` contains burst simulation samples for `n_anchor` anchor points,
+and can be in one of two layouts `L`.
+
+In the `L=Contiguous` layout, the `data` is an `Array{T,3}`, i.e.,
+the number of samples `n_samples` is equal for each anchor,
+and the `data` has the shape `(d, n_samples, n_anchors)`.
+
+In the `L=Jagged` case, the `data` is an `Vector{Array{T,2}}`, i.e.,
+each anchor may have a different number of samples,
+it is `length(data) = n_anchors`, and `size(data[i]) = (d, n_samples_i)`.
+
+Optionally, `weights` can be specified that give each sample in `data` a weight.
+Thus, the layout and shape of `weights` has to exactly match the `data`.
+
+The type of data points is `T<:Real`, and the type of the weights is `W<:Real`
+if weights were provided and `W=Nothing` otherwise.
+"""
+struct TransitionDistanceProblem{T<:Real,W<:Union{Real,Nothing},L<:DataLayout}
+    data::Union{ContiguousData{T},JaggedData{T}}
+    weights::Union{ContiguousData{W},JaggedData{W},Nothing}
+
+    function Problem(
+        data::Union{ContiguousData{T},JaggedData{T}},
+        weights::Union{ContiguousData{W},JaggedData{W},Nothing},
+    ) where {T<:Real,W<:Real}
+        L = data isa ContiguousData ? Contiguous : Jagged
+
+        if data isa JaggedData
+            # check that all `d` are the same
+            d = size(data[1], 1)
+            for x in data
+                size(x, 1) == d ||
+                    throw(ArgumentError("dimension `d` in data must not change"))
+            end
+        end
+
+        if isnothing(weights)
+            return new{T,Nothing,L}(data, nothing)
+        end
+
+        if (weights isa ContiguousData) != (data isa ContiguousData)
+            throw(ArgumentError("mixed layouts of data and weights"))
+        end
+
+        if L === Contiguous
+            size(data) == size(weights) ||
+                throw(ArgumentError("shapes of data and weights do not match"))
+            return new{T,W,L}(data, nothing)
+        end
+
+        # L === Jagged
+        # check same n_anchors
+        length(data) == length(weights) ||
+            throw(ArgumentError("lenghts of data and weights do not match"))
+
+        # check same sample sizes
+        for (x, y) in zip(data, weights)
+            size(x) == size(y) ||
+                throw(ArgumentError("shapes of data and weights do not match"))
+        end
+        return new{T,W,L}(data, nothing)
+    end
+end
+
+TransitionDistanceProblem(data; weights=nothing) = TransitionDistanceProblem(data, weights)
+
 """
     TransitionDistanceResult{T<:Real}
 
