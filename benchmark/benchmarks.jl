e9bcb9167f878ab5f673f30cc812b0b2abe731d2
Marvin Lücke <31667561+lueckem@users.noreply.github.com>
2026-08-14 11:47:44 +0200

    test(Trajectories): add benchmark for farthest point sampling

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -5,4 +5,7 @@ const SUITE = BenchmarkGroup()
 include("benchmarks_td.jl")
 SUITE["td"] = BenchmarkTD.SUITE
 
+include("benchmarks_trajs.jl")
+SUITE["trajs"] = BenchmarkTrajs.SUITE
+
 run_benchmarks() = run(SUITE; verbose=true, seconds=5)
